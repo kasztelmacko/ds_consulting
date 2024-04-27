7b0e211a02ec10d01bbd2b46aea732db2c55c109
kasztelmacko <m.kasztelani@student.uw.edu.pl>
2024-04-27 12:07:53 +0200

    more conversions of the dataset

--- etl.py
+++ etl.py
@@ -26,13 +26,13 @@ def remove_columns(data, columns):
 def extract_month_year(data, date_column):
     '''
     Function to extract month and year from a date column, and convert it to a numeric value
+    There is a problem with saving August, so it is replaced with correct value
     '''
-    data[date_column] = data[date_column].str.replace(r'(st|nd|rd|th)', '')
-    data[date_column] = pd.to_datetime(data[date_column], errors='coerce')
-    data[date_column] = data[date_column].dt.strftime('%B %Y')
+    data[date_column] = data[date_column].str.replace(r'(st|nd|rd|th|)', '', regex=True).str.replace('Augu', 'August')
+    data[date_column] = pd.to_datetime(data[date_column], format='%d %B %Y')
     return data
 
-def normalize_column_Route(data):
+def convert_Route(data):
     '''
     Function to normalize a column in a dataset (change Route to departure and arrival)
     '''
@@ -49,11 +49,38 @@ def fill_Route(data):
     data['Route'] = data['Route'].fillna('Unknown')
     return data
 
+def convert_AirlineName(data):
+    '''
+    Function to convert AirlineName as it stores same information as Slug
+    '''
+    data['AirlineName'] = data['AirlineName'].str.replace(' ', '-').replace("'","").str.lower()
+    return data
+
+def conver_Verified(data):
+    '''
+    Function to convert Verified Flighhts column to boolean
+    '''
+    data['TripVerified'] = data['TripVerified'].replace('NotVerified', 'Not Verified')
+    data['TripVerified'] = data['TripVerified'].apply(lambda x: 'Not Verified' if x not in ['Trip Verified', 'Not Verified'] else x)
+    data['TripVerified'] = data['TripVerified'].map({'Trip Verified': 1, 'Not Verified': 0})
+    return data
+
 if __name__ == '__main__':
     data = load_data('data/AirlineReviews.csv')
     get_size(data)
     data = extract_month_year(data, 'DatePub')
     data = fill_Route(data)
-    data = normalize_column_Route(data)
-    data = remove_columns(data, ['unique_id', 'Route'])
+    data = convert_Route(data)
+    data = convert_AirlineName(data)
+    data = conver_Verified(data)
+    '''
+    Remove columns that are not needed
+    unique_id: no value added
+    Route: replaced with Departure and Arrival
+    Slug: same as AirlineName
+    Aircraft: too much NaN values
+    Title: no added value to Review
+    DateFlown: Reviews are usually given after few days of the flight, and DatePub has no NaN values
+    '''
+    data = remove_columns(data, ['unique_id', 'Route', "Slug", "Aircraft", "Title", "DateFlown"])
     print(data.head(5))
